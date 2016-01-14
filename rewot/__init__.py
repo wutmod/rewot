9b71e7adfc270018228c82d81d31e26840257864
rascul <root@rascul.io>
2016-01-14 06:54:27 -0500

    mechanism to set debug mode

--- rewot/__init__.py
+++ rewot/__init__.py
@@ -1,5 +1,15 @@
+from os import getenv
+
 from flask import Flask
 
 app = Flask(__name__, static_url_path="")
+
+# debug mode will be enabled if the environment variable REWOT_DEBUG=true,
+# or if run via runserver.py
+if getenv("REWOT_DEBUG"):
+	if getenv("REWOT_DEBUG") == "true":
+		from werkzeug.debug import DebuggedApplication
+		app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+		app.config["DEBUG"] = True
 
 import rewot.views
